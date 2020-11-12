64f640a716a25a1c66f8e586804603b9040e035a
linkzepp <adriftchen@gmail.com>
2020-11-12 16:28:14 +0800

    完成發票新增功能及資料表建置

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -2,5 +2,28 @@
 //撰寫新增消費發票的程式碼
 //將發票的號碼及相關資訊寫入資料庫
 
+// foreach($_POST as $key => $value){
+//     echo "欄位".$key."==值" .$value."<br>";把此行換成下面方式:將變數輸出結果再丟入
+//     $tmp[]=$key;
+//   }
+
+// include_once "../base.php"
+$dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
+$pdo=new PDO($dsn,'root','');
+
+echo "<pre>";
+print_r(array_keys($_POST));
+echo "</pre>";
+
+
+$sql="insert into invoices (`".implode("`,`",array_keys($_POST))."`) values('".implode("','",$_POST)."')";
+//insert 前為""指欄位名稱，value內為值放單引號''
+echo $sql;
+$pdo->exec($sql); //exec為新增一筆資料
+
+echo "新增完成";
+// header("location:../index.php"); 
+//此檔案位於上一層
+
 
 ?>
