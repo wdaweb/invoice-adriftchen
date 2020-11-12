b094a511d250e7e4be87d979bc178b9871e14414
linkzepp <adriftchen@gmail.com>
2020-11-12 14:39:28 +0800

    完成首頁畫面設計

--- index.php
+++ index.php
@@ -5,8 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>統一發票紀錄及對獎系統</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    
 </head>
 <body>
+
+<?php
+
+if(isset($_GET['do'])){
+
+    $file=$_GET['do'].".php";
+    include $file;
+    include "invoice.php";
+
+}else{
+
+    include "main.php";
+}
+//include 有畫面之後就不用再打基礎指令
+?>
 
 </body>
 </html>
