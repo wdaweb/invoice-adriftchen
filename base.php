ada519196fb82ad255479e142f297cc3d0dc80fa
linkzepp <adriftchen@gmail.com>
2020-11-13 09:29:54 +0800

    base.php 函式修正

--- base.php
+++ base.php
@@ -5,7 +5,7 @@
 $dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
 $pdo=new PDO($dsn,'root','');
 
-date_default_timezone_get("Asia/Taipei");
+date_default_timezone_set("Asia/Taipei");
 session_start();
 
 ?>
