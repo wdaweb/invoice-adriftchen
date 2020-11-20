fcd3dbba02a1a26aedc3a097228faa9aa74857e0
linkzepp <adriftchen@gmail.com>
2020-11-20 11:28:39 +0800

    完成find()函式撰寫，可以代入id或是陣列做為查詢條件

--- test.php
+++ test.php
@@ -3,44 +3,55 @@
 include_once "base.php";
 
 
-//上面的指令丟到function裡會組成句子 select * from where id='9';
-//$row=$pdo->query("select * from where id='9'")->fetch();
+
+//"select * from invoices where id='9'";
+//$row=$pdo->query("select * from invoices where id='9'")->fetch();
 //$res=回傳的id為9的發票內容
-//以上直接用pdo有錯誤訊息跑不出來:pdo沒定義
-//funciton用pdo要設區域性global pdo
+echo implode(" && ",['欄位1'=>'值1','欄位2'=>'值2','id'=>'9']);
+echo "<br>";
+echo "欄位1='值1' && 欄位2='值2' && id='9'";
+echo "<hr>";
+$array=['欄位1'=>'值1','欄位2'=>'值2','id'=>'9'];
+echo "<hr>";
 
-function find($table,$id){
-  global $pdo;
-
-  if(is_numeric($id)){
-    $sql="select * from $table where id='$id'";
-  }else{
-    $sql="select * from $table where $id";
-  }
-
-  $row=$pdo->query($sql)->fetch();
-
-  return $row;
+//利用一個暫時的陣列來存放語句的片段
+foreach($array as $key => $value){
+    $tmp[]=sprintf("`%s`='%s'",$key,$value);
+    //$tmp[]="`".$key."`='".$value."'";
 }
 
-// function find2($table,$def){
-//   global $pdo;
+print_r($tmp);
+echo "<br>";
 
-//   $sql="select * from $table where $def";
-//   $row=$pdo->query($sql)->fetch();
+//使用implode把暫時陣列中的語句串回SQL語法
+echo implode(" && ",$tmp);
 
-//   return $row;
-// }
+echo "<br>";
+function find($table,$id){
+    global $pdo;
+    $sql="select * from $table where "; //把重複的句子拉到前面
+    if(is_array($id)){
+        foreach($id as $key => $value){
+            $tmp[]=sprintf("`%s`='%s'",$key,$value);
+            //$tmp[]="`".$key."`='".$value."'";
+        }
+        $sql=$sql.implode(' && ',$tmp);
+    }else{
+        $sql=$sql . " id='$id' ";
+    }
+    $row=$pdo->query($sql)->fetch();
 
-$row=find('invoices',11); /*find()本身是一個變數, */
+    return $row;
+}
+
+$row=find('invoices',5);
 echo $row['code'].$row['number']."<br>";
 
-$row=find('invoices',"code='AA'&& number='30292747'"); 
+$row=find('invoices',['code'=>'KJ','number'=>'69954161']);
 echo $row['code'].$row['number']."<br>";
 
-$row=find('invoices',27); 
+$row=find('invoices',17);
 echo $row['code'].$row['number']."<br>";
-
 
 
 ?>
